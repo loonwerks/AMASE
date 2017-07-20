145bae6da788cc9908b1c1e0761cd3f63a409dd2
User <dkstewar@umn.edu>
2017-07-20 11:10:03 -0500

    DS: Added parser, contentassist, linkingservice classes

---
+++ Grammar2/edu.umn.cs.crisys.safety.ui/src/edu/umn/cs/crisys/safety/parsing/SafetyAnnexParser.java
@@ -0,0 +1,49 @@
+package edu.umn.cs.crisys.safety.parsing;
+
+import org.osate.aadl2.AnnexLibrary;
+import org.osate.annexsupport.AnnexParseUtil;
+import org.osate.annexsupport.AnnexParser;
+
+import com.google.inject.Injector;
+
+import edu.umn.cs.crisys.safety.parser.antlr.SafetyParser;
+import edu.umn.cs.crisys.safety.services.SafetyGrammarAccess;
+import edu.umn.cs.crisys.safety.ui.internal.SafetyActivator;
+
+import org.osate.aadl2.AnnexSubclause;
+import org.osate.aadl2.modelsupport.errorreporting.ParseErrorReporter;
+
+
+public class SafetyAnnexParser implements AnnexParser{
+
+	private SafetyParser parser;
+
+    protected SafetyParser getParser() {
+        if (parser == null) {
+            Injector injector = SafetyActivator.getInstance().getInjector(
+                    SafetyActivator.EDU_UMN_CS_CRISYS_SAFETY_SAFETY);
+            parser = injector.getInstance(SafetyParser.class);
+        }
+        return parser;
+    }
+    
+    protected SafetyGrammarAccess getGrammarAccess() {
+        return getParser().getGrammarAccess();
+    }
+	
+	@Override
+	public AnnexLibrary parseAnnexLibrary(String annexName, String source, String filename, int line, int column,
+			ParseErrorReporter errReporter) {
+		// TODO Auto-generated method stub
+		return (AnnexLibrary) AnnexParseUtil.parse(getParser(),source, getGrammarAccess().getAgreeLibraryRule(), filename, line,
+                column, errReporter);
+	}
+
+	@Override
+	public AnnexSubclause parseAnnexSubclause(String annexName, String source, String filename, int line, int column,
+			ParseErrorReporter errReporter) {
+		// TODO Auto-generated method stub
+		return (AnnexSubclause) AnnexParseUtil.parse(getParser(),source,getGrammarAccess().getAgreeSubclauseRule(),filename,line,column, errReporter);
+	}
+
+}
