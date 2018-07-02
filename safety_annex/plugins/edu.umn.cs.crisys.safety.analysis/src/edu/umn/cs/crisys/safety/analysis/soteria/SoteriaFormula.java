7a06e839e538c52a24acc1b703f174becd6953df
Liu <jliu29@ccanet.rockwellcollins.com>
2018-07-01 19:05:09 -0500

    added printing capability to soteria model element

--- safety_annex/plugins/edu.umn.cs.crisys.safety.analysis/src/edu/umn/cs/crisys/safety/analysis/soteria/SoteriaFormula.java
+++ safety_annex/plugins/edu.umn.cs.crisys.safety.analysis/src/edu/umn/cs/crisys/safety/analysis/soteria/SoteriaFormula.java
@@ -2,9 +2,10 @@ package edu.umn.cs.crisys.safety.analysis.soteria;
 
 import java.util.ArrayList;
 
+import edu.umn.cs.crisys.safety.analysis.ast.visitors.SoteriaAstVisitor;
 import jkind.Assert;
 
-public class SoteriaFormula {
+public class SoteriaFormula extends SoteriaAst {
 	public final String propertyName;
 	public ArrayList<SoteriaFormulaElem> formulaBody = new ArrayList<SoteriaFormulaElem>();
 
@@ -13,5 +14,10 @@ public class SoteriaFormula {
 		Assert.isNotNull(formulaBody);
 		this.propertyName = propertyName;
 		this.formulaBody = formulaBody;
+	}
+
+	@Override
+	public <T> T accept(SoteriaAstVisitor<T> visitor) {
+		return visitor.visit(this);
 	}
 }
--- safety_annex/plugins/edu.umn.cs.crisys.safety.analysis/src/edu/umn/cs/crisys/safety/analysis/soteria/SoteriaModel.java
+++ safety_annex/plugins/edu.umn.cs.crisys.safety.analysis/src/edu/umn/cs/crisys/safety/analysis/soteria/SoteriaModel.java
@@ -1,5 +1,12 @@
 package edu.umn.cs.crisys.safety.analysis.soteria;
 
-public class SoteriaModel {
+import edu.umn.cs.crisys.safety.analysis.ast.visitors.SoteriaAstVisitor;
+
+public class SoteriaModel extends SoteriaAst {
+
+	@Override
+	public <T> T accept(SoteriaAstVisitor<T> visitor) {
+		return visitor.visit(this);
+	}
 
 }
