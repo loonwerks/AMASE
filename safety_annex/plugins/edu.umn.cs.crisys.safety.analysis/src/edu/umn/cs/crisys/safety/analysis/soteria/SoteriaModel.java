22dd3a357f548956db581997238bb6fc7ec2f373
Liu <jliu29@ccanet.rockwellcollins.com>
2018-07-22 15:13:31 -0500

    add top level faults

--- safety_annex/plugins/edu.umn.cs.crisys.safety.analysis/src/edu/umn/cs/crisys/safety/analysis/soteria/SoteriaModel.java
+++ safety_annex/plugins/edu.umn.cs.crisys.safety.analysis/src/edu/umn/cs/crisys/safety/analysis/soteria/SoteriaModel.java
@@ -1,5 +1,7 @@
 package edu.umn.cs.crisys.safety.analysis.soteria;
 
+import java.util.ArrayList;
+
 import edu.umn.cs.crisys.safety.analysis.ast.visitors.SoteriaAstVisitor;
 import jkind.Assert;
 
@@ -7,15 +9,25 @@ public class SoteriaModel extends SoteriaAst {
 
 	public SoteriaCompLib compLib;
 	public final String includeStr = "#use \"top.ml\";;";
-
-	public SoteriaModel(SoteriaCompLib compLib) {
-		Assert.isNotNull(compLib);
-		this.compLib = compLib;
-	}
+	public ArrayList<SoteriaCompInst> compInstList = new ArrayList<SoteriaCompInst>();
+	public ArrayList<CompContractViolation> topLevelFaults = new ArrayList<CompContractViolation>();
 
 	@Override
 	public <T> T accept(SoteriaAstVisitor<T> visitor) {
 		return visitor.visit(this);
+	}
+
+	public void createCompInst() {
+
+	}
+
+	public void addCompLib(SoteriaCompLib compLib) {
+		Assert.isNotNull(compLib);
+		this.compLib = compLib;
+	}
+
+	public void addTopLevelFault(CompContractViolation contractViolation) {
+		topLevelFaults.add(contractViolation);
 	}
 
 }
