336da8b0a9150da03ccef02e2df746f64c61045b
Liu <jliu29@ccanet.rockwellcollins.com>
2018-08-18 13:54:16 -0500

    generated soteria fault tree fine for C0123, C123 Test Example

--- safety_annex/plugins/edu.umn.cs.crisys.safety.analysis/src/edu/umn/cs/crisys/safety/analysis/soteria/faultTree/SoteriaFaultTree.java
+++ safety_annex/plugins/edu.umn.cs.crisys.safety.analysis/src/edu/umn/cs/crisys/safety/analysis/soteria/faultTree/SoteriaFaultTree.java
@@ -1,6 +1,8 @@
 package edu.umn.cs.crisys.safety.analysis.soteria.faultTree;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import edu.umn.cs.crisys.safety.analysis.ast.visitors.SoteriaFTAstVisitor;
 
@@ -9,6 +11,7 @@ public class SoteriaFaultTree extends SoteriaFTAst {
 	public HashMap<String, SoteriaFTLeafNode> leafNodes = new HashMap<>();
 	public HashMap<String, SoteriaFTNonLeafNode> rootNodes = new HashMap<>();
 	public HashMap<String, SoteriaFTNonLeafNode> intermediateNodes = new HashMap<>();
+	public List<SoteriaFTNonLeafNode> sortedIntermediateNodes = new ArrayList<SoteriaFTNonLeafNode>();
 
 	public void addLeafNode(String nodeName, SoteriaFTLeafNode leafNode) {
 		leafNodes.put(nodeName, leafNode);
@@ -27,4 +30,38 @@ public class SoteriaFaultTree extends SoteriaFTAst {
 		return visitor.visit(this);
 	}
 
+	// some node's child nodes were added before those child nodes obtained their own child nodes
+	// walk through all intermediate nodes to fix any discrepancies
+	public void updateChildNodes() {
+		for (SoteriaFTNonLeafNode intermediateNode : intermediateNodes.values()) {
+			for (String childName : intermediateNode.childNodes.keySet()) {
+				if (intermediateNodes.containsKey(childName)) {
+					intermediateNode.addChildNode(childName, intermediateNodes.get(childName));
+				}
+			}
+		}
+	}
+
+	// sort through intermediate nodes to declare before use
+	public void sortIntermediateNodes() {
+		for (SoteriaFTNonLeafNode intermediateNode : intermediateNodes.values()) {
+			declareBeforeUse(intermediateNode);
+		}
+	}
+
+	private void declareBeforeUse(SoteriaFTNonLeafNode node) {
+		// if it's not yet in the sortedIntermediateNodes
+		if (!sortedIntermediateNodes.contains(node)) {
+			// go through each child node
+			// if a child node is not a leaf node
+			// declare before use that child node
+			for (SoteriaFTNode childNode : node.childNodes.values()) {
+				if (childNode instanceof SoteriaFTNonLeafNode) {
+					declareBeforeUse((SoteriaFTNonLeafNode) childNode);
+				}
+			}
+			// after going through the child node, add the node to sortedIntermediateNodes
+			sortedIntermediateNodes.add(node);
+		}
+	}
 }
