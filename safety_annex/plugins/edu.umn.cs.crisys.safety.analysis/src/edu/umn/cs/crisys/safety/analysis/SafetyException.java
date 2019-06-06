65580523c4e1b871b2fa3202ec4da198c7f969f8
Danielle Stewart <dkstewar@umn.edu>
2019-06-06 10:00:00 -0500

    DS: Safety Exceptions now show dialog box to user.

--- safety_annex/plugins/edu.umn.cs.crisys.safety.analysis/src/edu/umn/cs/crisys/safety/analysis/SafetyException.java
+++ safety_annex/plugins/edu.umn.cs.crisys.safety.analysis/src/edu/umn/cs/crisys/safety/analysis/SafetyException.java
@@ -1,13 +1,16 @@
 package edu.umn.cs.crisys.safety.analysis;
 
+import javax.swing.JOptionPane;
+
 public class SafetyException extends RuntimeException {
 
 	/**
-	 * SafetyException : this number may need to be changed. 
+	 * SafetyException : this number may need to be changed.
 	 */
 	private static final long serialVersionUID = 1L;
 
 	public SafetyException(String message){
-		super(message);
+		JOptionPane.showMessageDialog(null, message, "Safety Annex Error", JOptionPane.ERROR_MESSAGE);
+		// super(message);
 	}
 }
