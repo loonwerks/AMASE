8ff80fe0c86e0d7154f9a7947626415c9beda611
Liu <jliu29@ccanet.rockwellcollins.com>
2019-06-07 10:01:20 -0500

    updated pruning in mhs calculation

--- safety_annex/plugins/edu.umn.cs.crisys.safety.analysis/src/edu/umn/cs/crisys/safety/analysis/ast/visitors/AddFaultsToNodeVisitor.java
+++ safety_annex/plugins/edu.umn.cs.crisys.safety.analysis/src/edu/umn/cs/crisys/safety/analysis/ast/visitors/AddFaultsToNodeVisitor.java
@@ -121,6 +121,7 @@ public class AddFaultsToNodeVisitor extends AgreeASTMapVisitor {
 	public static double probabilityThreshold = 0.0;
 	public static boolean upperMostLevel = true;
 	public static ArrayList<FaultSetProbability> faultCombinationsAboveThreshold = new ArrayList<>();
+	public static Set<Set<String>> faultCombinationAboveThresholdStrs = new HashSet<Set<String>>();
 	public static boolean maxFaultHypothesis = false;
 	public static boolean probabilisticHypothesis = false;
 
@@ -143,6 +144,7 @@ public class AddFaultsToNodeVisitor extends AgreeASTMapVisitor {
 		probabilityThreshold = 0.0;
 		upperMostLevel = true;
 		faultCombinationsAboveThreshold.clear();
+		faultCombinationAboveThresholdStrs.clear();
 		maxFaultHypothesis = false;
 		probabilisticHypothesis = false;
 	}
@@ -1304,6 +1306,16 @@ public class AddFaultsToNodeVisitor extends AgreeASTMapVisitor {
 				}
 				index++;
 			}
+		}
+		// convert fault combination to str set
+		convertFaultCombinationToStrSet();
+	}
+
+	private void convertFaultCombinationToStrSet() {
+
+		for (FaultSetProbability faultCombination : faultCombinationsAboveThreshold) {
+			HashSet<String> faultCombinationSet = faultCombination.toStringSet();
+			faultCombinationAboveThresholdStrs.add(faultCombinationSet);
 		}
 	}
 
