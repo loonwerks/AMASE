87012cd17904ffbbffd5126696f68d6ccfb3f603
Danielle Stewart <dkstewar@umn.edu>
2020-04-10 10:30:38 -0500

    Preference constant error fix.

--- safety_annex/plugins/edu.umn.cs.crisys.safety.analysis/src/edu/umn/cs/crisys/safety/analysis/preferences/PreferenceConstants.java
+++ safety_annex/plugins/edu.umn.cs.crisys.safety.analysis/src/edu/umn/cs/crisys/safety/analysis/preferences/PreferenceConstants.java
@@ -4,5 +4,5 @@ package edu.umn.cs.crisys.safety.analysis.preferences;
  * Constant definitions for plug-in preferences
  */
 public class PreferenceConstants {
-
+	public static String TEST_BOOL_EDITOR = "Test bool editor";
 }
