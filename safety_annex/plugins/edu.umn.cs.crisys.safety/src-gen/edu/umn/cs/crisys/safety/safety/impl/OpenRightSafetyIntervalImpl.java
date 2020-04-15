8764998339384e2b9507a682932d2ad9fc3e2623
Liu <jliu29@ccanet.rockwellcollins.com>
2020-04-15 15:47:55 -0500

    generated files from safety annex xtext update

---
+++ safety_annex/plugins/edu.umn.cs.crisys.safety/src-gen/edu/umn/cs/crisys/safety/safety/impl/OpenRightSafetyIntervalImpl.java
@@ -0,0 +1,40 @@
+/**
+ */
+package edu.umn.cs.crisys.safety.safety.impl;
+
+import edu.umn.cs.crisys.safety.safety.OpenRightSafetyInterval;
+import edu.umn.cs.crisys.safety.safety.SafetyPackage;
+
+import org.eclipse.emf.ecore.EClass;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Open Right Safety Interval</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * @generated
+ */
+public class OpenRightSafetyIntervalImpl extends SafetyIntervalImpl implements OpenRightSafetyInterval
+{
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected OpenRightSafetyIntervalImpl()
+  {
+    super();
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  protected EClass eStaticClass()
+  {
+    return SafetyPackage.Literals.OPEN_RIGHT_SAFETY_INTERVAL;
+  }
+
+} //OpenRightSafetyIntervalImpl
