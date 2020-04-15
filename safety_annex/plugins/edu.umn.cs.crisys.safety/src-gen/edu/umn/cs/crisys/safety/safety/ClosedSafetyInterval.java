a2df88c9f0b9fde74f8cbe6d932a451dcd5672c0
Liu <jliu29@ccanet.rockwellcollins.com>
2020-04-15 15:47:55 -0500

    generated files from safety annex xtext update

---
+++ safety_annex/plugins/edu.umn.cs.crisys.safety/src-gen/edu/umn/cs/crisys/safety/safety/ClosedSafetyInterval.java
@@ -0,0 +1,18 @@
+/**
+ */
+package edu.umn.cs.crisys.safety.safety;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Closed Safety Interval</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see edu.umn.cs.crisys.safety.safety.SafetyPackage#getClosedSafetyInterval()
+ * @model
+ * @generated
+ */
+public interface ClosedSafetyInterval extends SafetyInterval
+{
+} // ClosedSafetyInterval
---
+++ safety_annex/plugins/edu.umn.cs.crisys.safety/src-gen/edu/umn/cs/crisys/safety/safety/OpenRightSafetyInterval.java
@@ -0,0 +1,18 @@
+/**
+ */
+package edu.umn.cs.crisys.safety.safety;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Open Right Safety Interval</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see edu.umn.cs.crisys.safety.safety.SafetyPackage#getOpenRightSafetyInterval()
+ * @model
+ * @generated
+ */
+public interface OpenRightSafetyInterval extends SafetyInterval
+{
+} // OpenRightSafetyInterval
