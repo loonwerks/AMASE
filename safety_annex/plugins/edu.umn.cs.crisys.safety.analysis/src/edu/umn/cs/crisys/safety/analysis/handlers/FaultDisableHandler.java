c38973ee386839c7b6667d21d03cc6899892d9a6
Danielle Stewart <dkstewar@umn.edu>
2020-04-15 20:28:07 -0500

    Reorganized handlers and created new FaultDisableHandler.

---
+++ safety_annex/plugins/edu.umn.cs.crisys.safety.analysis/src/edu/umn/cs/crisys/safety/analysis/handlers/FaultDisableHandler.java
@@ -0,0 +1,10 @@
+package edu.umn.cs.crisys.safety.analysis.handlers;
+
+public class FaultDisableHandler extends VerifyHandler {
+
+	@Override
+	protected String getJobName() {
+		return "Fault disable menu";
+	}
+
+}
---
+++ safety_annex/plugins/edu.umn.cs.crisys.safety.analysis/src/edu/umn/cs/crisys/safety/analysis/handlers/VerifyFaultsHandler.java
@@ -0,0 +1,10 @@
+package edu.umn.cs.crisys.safety.analysis.handlers;
+
+public class VerifyFaultsHandler extends VerifyHandler {
+
+	@Override
+	protected String getJobName() {
+		return "Safety Analysis results";
+	}
+
+}
