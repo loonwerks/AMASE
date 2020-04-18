09dd9b9cff6a97683fce03cb96cdb7a7f1eda826
Danielle Stewart <dkstewar@umn.edu>
2020-04-18 12:34:54 -0500

    Added new handlers for updates to menu items.

---
+++ safety_annex/plugins/edu.umn.cs.crisys.safety.analysis/src/edu/umn/cs/crisys/safety/analysis/handlers/FaultsVerifySingleHandler.java
@@ -0,0 +1,7 @@
+package edu.umn.cs.crisys.safety.analysis.handlers;
+
+import com.rockwellcollins.atc.agree.analysis.handlers.VerifySingleHandler;
+
+public class FaultsVerifySingleHandler extends VerifySingleHandler {
+
+}
