c4824481c3c8c60ad4ca17122bd45329947b73c6
Danielle Stewart <dkstewar@umn.edu>
2020-04-18 13:25:06 -0500

    All new handlers set up.

--- safety_annex/plugins/edu.umn.cs.crisys.safety.analysis/src/edu/umn/cs/crisys/safety/analysis/handlers/FaultsVerifySingleHandler.java
+++ safety_annex/plugins/edu.umn.cs.crisys.safety.analysis/src/edu/umn/cs/crisys/safety/analysis/handlers/FaultsVerifySingleHandler.java
@@ -1,7 +1,37 @@
 package edu.umn.cs.crisys.safety.analysis.handlers;
 
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.MenuItem;
+
 import com.rockwellcollins.atc.agree.analysis.handlers.VerifySingleHandler;
+
+import edu.umn.cs.crisys.safety.analysis.ast.visitors.AddFaultsToNodeVisitor;
+import edu.umn.cs.crisys.safety.analysis.transform.AddFaultsToAgree;
 
 public class FaultsVerifySingleHandler extends VerifySingleHandler {
 
+	private static MenuItem item;
+
+	@Override
+	public Object execute(ExecutionEvent event) {
+		Event selEvent = (Event) event.getTrigger();
+		item = (MenuItem) selEvent.widget;
+		AddFaultsToAgree.setTransformFlag(item);
+		// clear static variables before each run
+		AddFaultsToNodeVisitor.init();
+		return super.execute(event);
+	}
+
+	/**
+	 * (non-Javadoc)
+	 *
+	 * @see edu.umn.cs.crisys.safety.analysis.handlers.AadlHandler#getJobName()
+	 *
+	 * Output job name (will see it at the bottom of Osate2 after selected)
+	 */
+	@Override
+	protected String getJobName() {
+		return "Fault analysis: single layer";
+	}
 }
